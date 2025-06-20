72f66818e5945c80e57d6629d7a8eb61be3b80da
dambosin <dambosin@gmail.com>
2025-06-21 01:20:10 +0300

    chore(project): test pino logger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import {config} from 'dotenv';
 import {Bot} from './bot';
-import {Logger} from './logger/logger';
+import {Logger} from './logger/Logger';
+import pino from 'pino';
 
 config();
 
+const pinoLogger = pino({
+  level: 'info',
+  transport: {
+    targets: [
+      {target: 'pino-pretty', level: 'info'},
+      {
+        target: 'pino/file',
+        options: {destination: 'logs/app.log', mkdir: true, interval: '1d'},
+        level: 'info',
+      },
+    ],
+  },
+});
+
 const token = process.env.BOT_TOKEN;
-const logger = new Logger();
+const logger = new Logger(pinoLogger);
 
 if (!token) {
   logger.fatal('BOT_TOKEN is not set');
